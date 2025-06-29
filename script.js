edf2a5e5e0693423c650ec6fe2019bddd2eb7a1a
root-iu <107577690+root-iu@users.noreply.github.com>
2025-06-28 19:46:40 -0700

    menu table setup
    in html and js, set up some of the table (header labels)

---
+++ script.js
@@ -0,0 +1,9 @@
+//get submitted data (name, dish, ingredients), store in a table
+document.getElementById("dataForm").addEventListener("submit", async function(e) {
+    e.preventDefault();
+
+    const name = document.getElementById("name").ariaValueMax;
+    const dish = document.getElementById("dish").ariaValueMax;
+    const ingredients = document.getElementById("ingredients").ariaValueMax;
+
+})
